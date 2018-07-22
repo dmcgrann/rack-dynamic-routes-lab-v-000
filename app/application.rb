6feb48ffd0a9f148a7a9d4018182d002696f6546
David McGrann <dpmcgrann@gmail.com>
2018-07-22 21:50:43 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,10 @@
 class Application
  
-  @@items = ["Apples","Carrots","Pears"]
  
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     
-    if req.path == "/items/"
+    if req.path == "/items/<ITEM NAME>"
+      resp.write "#{item.price}"
+    else
