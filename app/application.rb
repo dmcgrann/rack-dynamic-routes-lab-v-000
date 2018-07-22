3a93ebc3548bfa528a6d15e6f9c491971851906c
David McGrann <dpmcgrann@gmail.com>
2018-07-22 22:07:15 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
       resp.write item.price
-    else
+    elsif req.path != "/songs"
       resp.write "Route not found."
       resp.status = 404
     end
